f5f6c4294826f56948ef57348c3a602bf013d0b4
joshiaj7 <joshia.tan7@gmail.com>
2020-12-16 14:44:25 +0700

    add env var

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/joshiaj7/tax-calculator
 go 1.14
 
 require (
+	github.com/joho/godotenv v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	gorm.io/driver/postgres v1.0.2
 	gorm.io/gorm v1.20.2
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,36 @@
 package config
 
 import (
+	"fmt"
+
+	"github.com/joho/godotenv"
 	"github.com/joshiaj7/tax-calculator/internal/model"
+	"github.com/kelseyhightower/envconfig"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+type config struct {
+	hostname string `envconfig:"HOSTNAME"`
+	user     string `envconfig:"USER"`
+	password string `envconfig:"PASSWORD"`
+	dbName   string `envconfig:"NAME"`
+	port     string `envconfig:"PORT"`
+}
 
 // DB for global use
 var DB *gorm.DB
 
 // SetupDB to setup db
 func SetupDB() (err error) {
-	dsn := "host=localhost user=postgres password=password123 dbname=postgres port=5432 sslmode=disable"
+	godotenv.Load(".env")
+
+	var c config
+	err = envconfig.Process("DB", &c)
+
+	format := "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable"
+
+	dsn := fmt.Sprintf(format, c.hostname, c.user, c.password, c.dbName, c.port)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
